51a140e4a160aac4ddc3e5dd8c1c42def6506cc3
Sylvain Chen <sychen@redhat.com>
2019-08-29 16:44:50 +0200

    Add OAuth to alertmanager, replaced oauth image, replace deployment configs by statefulsets, add HA alertmanagers and prometheus

--- set-up.sh
+++ set-up.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 oc new-project prometheus
-oc create -f monitoring/pvc.yaml
 oc create -f monitoring/template.yaml
-oc process prometheus -p NAMESPACE=prometheus | oc create -f -
+oc process prometheus -p NAMESPACE=prometheus -v WILDCARD_DOMAIN=apps.example.com | oc create -f -
